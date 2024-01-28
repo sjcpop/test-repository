c78b16e25c591d615a58706b6c4914b9f6c374cd
SJ <cheong.sijian@gmail.com>
2024-01-28 18:47:49 +0800

    updated main function

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ var rollDice = function:(){
 }
 
 var main = function (input) {
-  var numberOfWeeks = input;
-  var minutesInAnHour = 60
-  var hoursInADay = 24
-  var daysInAWeek = 7
-  var numberOfMinutes = numberOfWeeks * daysInAWeek * hoursInADay * minutesInAnHour
-  var myOutputValue = "Total number of minutes in " + input + "is" + numberOfMinutes + " weeks";
+  var myOutputValue = "hello word";
   return myOutputValue;
 };
