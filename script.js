aa1216832e3a99f385ca8184a70750340a7d2b4d
SJ <cheong.sijian@gmail.com>
2024-01-28 18:27:16 +0800

    added roll dice function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var rollDice = function:(){
+  var randomNumber = Math.random() * 6;
+  var randomInteger = Math.floor(randomNumber);
+  var randomDiceRoll = randomInteger +1;
+  return randomDiceRoll;
+}
+
 var main = function (input) {
   var numberOfWeeks = input;
   var minutesInAnHour = 60
